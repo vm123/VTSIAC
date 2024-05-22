b71f0b5946e28ea34b4e4deef3c0bd4228c3ff48
Vijay Mannava <vijaym@vt.edu>
2024-05-22 19:12:56 -0400

    Initial commit

---
+++ Pages/Interest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace spanishImmersion.Pages
+{
+    public class InterestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+/*var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();*/
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts(); // The default HSTS value is 30 days.
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+// Enable serving of default files like index.html
+app.UseDefaultFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
